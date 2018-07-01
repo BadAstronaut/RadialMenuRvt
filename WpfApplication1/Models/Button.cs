872a542c837f1fe709c1c91913149f2d9bd63abb
BADASTRONAUT\Owner <rzepeda1@uc.cl>
2018-07-01 16:28:45 -0600

    Add project files.

---
+++ WpfApplication1/Models/Button.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1.Models
+{
+    class Button
+    {
+        public string Name { get; set; }
+        public string KeyboardShortcut { get; set; }
+        public string Tooltip { get; set; }
+        public Image Icon { get; set; }
+
+        public Button()
+        {
+            Name = "Copy";
+            KeyboardShortcut = "CO";
+            Tooltip = "Copiar Elementos";
+        }
+    }
+}
---
+++ WpfApplication1/Views/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RadialMenu;
+using System.Windows.Resources;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            #region "Sound"
+            #endregion
+            var point = GetMousePositionWindowsForms();
+            this.Left = point.X - 175;
+            this.Top = point.Y - 175;
+            InitializeComponent();
+
+        }
+        private void PlayOpeningSound()
+        {
+            Uri soundUrl = new Uri(@"pack://application:,,,/RadialApplication;component/icons/opensound.wav");
+            StreamResourceInfo sri = Application.GetResourceStream(soundUrl);
+            System.Media.SoundPlayer startSoundPlayer = new System.Media.SoundPlayer(sri.Stream);
+            startSoundPlayer.Play();
+
+        }
+
+        private void RadialMenuCentralItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+        public static Point GetMousePositionWindowsForms()
+        {
+            var point = System.Windows.Forms.Control.MousePosition;
+            return new Point(point.X, point.Y);
+        }
+        public void Dispose()
+        {
+            this.Close();
+        }
+        #region "Base Commands"
+        #endregion
+    }
+}
